9f00358928fca94adf08830f2b9348947265a163
Aidan Dunfield <aidan.dunfield@gmail.com>
2022-10-14 17:37:04 -0300

    Allowed to switch from create account to login page

--- app/src/main/java/com/example/gorup16project/CreateAccount.java
+++ app/src/main/java/com/example/gorup16project/CreateAccount.java
@@ -2,16 +2,30 @@ package com.example.gorup16project;
 
 import android.os.Bundle;
 import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CreateAccount extends AppCompatActivity {
-
+    Button switchToLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_account);
+
+
+        switchToLogin = findViewById(R.id.backToLoginButton);
+        switchToLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switchActivities();
+            }
+        });
     }
 
-
+    private void switchActivities() {
+        Intent switchToLogin = new Intent(CreateAccount.this, MainActivity.class);
+        startActivity(switchToLogin);
+    }
 }
