30457759bce2e6b8dd00da21faf62d1493dd69bd
Anastasiya Gurevich <an439014@dal.ca>
2022-10-16 17:38:44 -0300

    Back to login page button test case working on create account

--- app/src/androidTest/java/com/example/gorup16project/CreateAccountEspressoTest.java
+++ app/src/androidTest/java/com/example/gorup16project/CreateAccountEspressoTest.java
@@ -1,6 +1,7 @@
 package com.example.gorup16project;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
@@ -55,6 +56,13 @@ public class CreateAccountEspressoTest {
         onView(withId(R.id.editTextTextPersonName3)).check(matches(withText(R.string.EMPTY_STRING)));
 
     }
+    @Test
+    public void checkIfMovedToLoginPage() {
+        onView(withId(R.id.backToLoginButton)).perform(click());
+        onView(withId(R.id.editTextTextEmailAddress3)).check(matches(withText(R.string.EMPTY_STRING)));
+        onView(withId(R.id.editTextTextPassword3)).check(matches(withText(R.string.EMPTY_STRING)));
+    }
+
 
 
 
