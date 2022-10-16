28f1a6efebd83a0c8ae2f56f435ad75067f18000
Zachary Peters <zc543859@dal.ca>
2022-10-16 20:59:18 -0300

    added activity switch tests, updated gradle

--- app/src/androidTest/java/com/example/gorup16project/CreateAccountEspressoTest.java
+++ app/src/androidTest/java/com/example/gorup16project/CreateAccountEspressoTest.java
@@ -2,13 +2,18 @@ package com.example.gorup16project;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.init;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.assertEquals;
 
 import android.content.Context;
 
+import androidx.test.espresso.action.ViewActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
@@ -29,16 +34,10 @@ public class CreateAccountEspressoTest {
     @Rule
     public ActivityScenarioRule<CreateAccount> myRule2 = new ActivityScenarioRule<>(CreateAccount.class);
 
-
-
-
-
     @AfterClass
     public static void tearDown() {
         System.gc();
     }
-
-
 
     @Test
     public void useAppContext() {
@@ -46,7 +45,6 @@ public class CreateAccountEspressoTest {
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.gorup16project", appContext.getPackageName());
     }
-
 
     @Test
     public void checkIfCreateAccountPageIsVisible() {
@@ -56,15 +54,36 @@ public class CreateAccountEspressoTest {
         onView(withId(R.id.editTextTextPersonName3)).check(matches(withText(R.string.EMPTY_STRING)));
 
     }
+
     @Test
     public void checkIfMovedToLoginPage() {
+        init();
         onView(withId(R.id.backToLoginButton)).perform(click());
-        onView(withId(R.id.editTextTextEmailAddress3)).check(matches(withText(R.string.EMPTY_STRING)));
-        onView(withId(R.id.editTextTextPassword3)).check(matches(withText(R.string.EMPTY_STRING)));
+        intended(hasComponent(MainActivity.class.getName()));
     }
 
+    @Test
+    public void createAccount() {
+        init();
+        onView(withId(R.id.editTextTextPersonName)).perform(typeText("First"));
+        onView(withId(R.id.editTextTextPersonName3)).perform(typeText("Last"));
+        onView(withId(R.id.editTextTextEmailAddress2)).perform(typeText("123@email.ca"));
+        onView(withId(R.id.editTextTextPassword)).perform(typeText("password")).perform(ViewActions.closeSoftKeyboard());
 
+        onView(withId(R.id.button)).perform(click());
 
+        intended(hasComponent(MainActivity.class.getName()));
+    }
 
+    @Test
+    public void createAccountAndLogin() {
+        init();
+        createAccount();
 
+        onView(withId(R.id.editTextTextEmailAddress3)).perform(typeText("123@email.ca"));
+        onView(withId(R.id.editTextTextPassword3)).perform(typeText("password"));
+
+        onView(withId(R.id.buttonLogin)).perform(click());
+        intended(hasComponent(WelcomeActivity.class.getName()));
+    }
 }
