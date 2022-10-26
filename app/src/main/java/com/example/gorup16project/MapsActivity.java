d1b94c7df2009ba5f598fe36233e3cd65662512b
Anastasiya Gurevich <an439014@dal.ca>
2022-10-26 20:57:35 -0300

    User location set up

---
+++ app/src/main/java/com/example/gorup16project/MapsActivity.java
@@ -0,0 +1,15 @@
+package com.example.gorup16project;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+
+    }
+}
