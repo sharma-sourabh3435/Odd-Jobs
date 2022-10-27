50d259fabd366cccfb2b9d9c17a58cb6f88e78c4
Anastasiya Gurevich <an439014@dal.ca>
2022-10-26 21:46:37 -0300

    maps activity set up

--- app/src/main/java/com/example/gorup16project/MapsActivity.java
+++ app/src/main/java/com/example/gorup16project/MapsActivity.java
@@ -1,15 +1,93 @@
 package com.example.gorup16project;
 
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+    private static final Integer REQUEST_CODE = 111;
+    private GoogleMap mMap;
+    private ActivityMapsBinding binding;
+    SupportMapFragment mapFragment;
+    private FusedLocationProviderClient client;
 
     @Override
-    public void onMapReady(@NonNull GoogleMap googleMap) {
-
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+// Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        client = LocationServices.getFusedLocationProviderClient(MapsActivity.this);
+        if(ActivityCompat.checkSelfPermission(MapsActivity.this,Manifest.permission.ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED){
+            getCurrentLocation();
+        }else {
+            ActivityCompat.requestPermissions(MapsActivity.this,new
+                    String[]{Manifest.permission.ACCESS_FINE_LOCATION},REQUEST_CODE);
+        }
     }
+
+
+    private void getCurrentLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        Task<Location> task = client.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if(location != null) {
+                    mapFragment.getMapAsync(new OnMapReadyCallback() {
+                        @Override
+                        public void onMapReady(GoogleMap googleMap) {
+                            LatLng latlng = new LatLng(location.getLatitude(),location.getLongitude());
+                            MarkerOptions markerOptions = new MarkerOptions().position(latlng).title("You are here");
+                            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latlng,14));
+                            googleMap.addMarker(markerOptions).showInfoWindow();
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull
+            int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == REQUEST_CODE){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                getCurrentLocation();
+            }else{
+                Toast.makeText(this, "Permission Denied by user !!!", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+
+
 }
+
