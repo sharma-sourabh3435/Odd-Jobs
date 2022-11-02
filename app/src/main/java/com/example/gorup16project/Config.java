dcc6fa4e8848ab6f416c865c9351526fd0ab3c9e
Aidan Dunfield <aidan.dunfield@gmail.com>
2022-11-02 19:48:27 -0300

    Added PayPal integration to project

---
+++ app/src/main/java/com/example/gorup16project/Config.java
@@ -0,0 +1,6 @@
+package com.example.gorup16project;
+
+public class Config {
+
+    public static final String PAYPAL_CLIENT_ID = "AQojGCR2kxh7ZOzfitdXiOqUyof4euoApUfIgnczDWzk3SoyoieYaWEex0jouoQhCfdWZyJ6dM9Nm5ms";
+}
---
+++ app/src/main/java/com/example/gorup16project/PayPal.java
@@ -0,0 +1,113 @@
+package com.example.gorup16project;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.result.ActivityResultLauncher;
+
+import static android.content.ContentValues.TAG;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.paypal.android.sdk.payments.PayPalConfiguration;
+import com.paypal.android.sdk.payments.PayPalPayment;
+import com.paypal.android.sdk.payments.PayPalService;
+import com.paypal.android.sdk.payments.PaymentActivity;
+import com.paypal.android.sdk.payments.PaymentConfirmation;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.math.BigDecimal;
+
+public class PayPal extends AppCompatActivity {
+
+    ActivityResultLauncher activityResultLauncher;
+
+    private static final int PAYPAL_REQUEST_CODE = 555;
+    private static PayPalConfiguration config;
+    Button btnPayNow;
+    EditText edtAmount;
+    TextView paymentTV;
+
+    String amount = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.paypal);
+        config = new PayPalConfiguration()
+                .environment(PayPalConfiguration.ENVIRONMENT_SANDBOX)
+                .clientId(Config.PAYPAL_CLIENT_ID);
+
+        edtAmount = findViewById(R.id.edtAmount);
+        btnPayNow = findViewById(R.id.btnPayNow);
+        // creating a variable for button, edit text and status tv.
+        paymentTV = findViewById(R.id.idTVStatus);
+        // initiallizing Activity Launcher
+        initializeActivityLauncher();
+
+        btnPayNow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                processPayment();
+            }
+        });
+    }
+
+    private void initializeActivityLauncher() {
+        // Initialize result launcher
+        activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                if (result.getResultCode() == RESULT_OK) {
+                    PaymentConfirmation confirmation = result.getData().getParcelableExtra(PaymentActivity.EXTRA_RESULT_CONFIRMATION);
+                    if (confirmation != null) {
+                        try {
+                            // Getting the payment details
+                            String paymentDetails = confirmation.toJSONObject().toString(4);
+                            // on below line we are extracting json response and displaying it in a text view.
+                            JSONObject payObj = new JSONObject(paymentDetails);
+                            String payID = payObj.getJSONObject("response").getString("id");
+                            String state = payObj.getJSONObject("response").getString("state");
+                            paymentTV.setText("Payment " + state + "\n with payment id is " + payID);
+                        } catch (JSONException e) {
+                            // handling json exception on below line
+                            Log.e("Error", "an extremely unlikely failure occurred: ", e);
+                        }
+                    }
+
+                    //Toast.makeText(this, "Cancel", Toast.LENGTH_SHORT).show();
+                } else if (result.getResultCode() == PaymentActivity.RESULT_EXTRAS_INVALID){
+                    Log.d(TAG,"Launcher Result Invalid");
+                } else if (result.getResultCode() == Activity.RESULT_CANCELED) {
+                    Log.d(TAG, "Launcher Result Cancelled");
+                }
+            }
+        });
+    }
+
+    private void processPayment() {
+        // Getting amount from editText
+        amount = edtAmount.getText().toString();
+        // Creating Paypal payment
+        PayPalPayment payPalPayment = new PayPalPayment(new BigDecimal(String.valueOf(amount)),"CAD","Purchase Goods",PayPalPayment.PAYMENT_INTENT_SALE);
+        // Creating Paypal Payment activity intent
+        Intent intent = new Intent(this, PaymentActivity.class);
+        // Adding paypal configuration to the intent
+        intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION,config);
+        // Adding paypal payment to the intent
+        intent.putExtra(PaymentActivity.EXTRA_PAYMENT,payPalPayment);
+        // Starting Activity Request launcher
+        activityResultLauncher.launch(intent);
+    }
+}
