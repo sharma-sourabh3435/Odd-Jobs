af1ebb305d7ba3608fd41a77f938c3033b138827
Anastasiya Gurevich <an439014@dal.ca>
2022-11-28 23:43:42 -0400

    code for profile page

--- app/src/main/java/com/example/gorup16project/ProfilePage.java
+++ app/src/main/java/com/example/gorup16project/ProfilePage.java
@@ -1,2 +1,35 @@
-package com.example.gorup16project;public class ProfilePage {
+package com.example.gorup16project;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProfilePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_page);
+
+        Button switchToLocation = findViewById(R.id.Location);
+        switchToLocation.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            switchActivities();
+        }
+    });
+
+
+
+}
+
+    private void switchActivities() {
+        Intent switchToCreate = new Intent(ProfilePage.this, MapsActivity.class);
+        startActivity(switchToCreate);
+    }
+
+
 }
